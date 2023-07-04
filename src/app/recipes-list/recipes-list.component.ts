d4d4c52aa0cd0314f2bd9ff7f7e26e00376841e2
Sean Campbell <sean.campbell3161@gmail.com>
2023-07-03 23:57:52 -0500

    update list change detection, add primeflex

--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RecipesService } from '../core/services/recipes.service';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.scss']
+  styleUrls: ['./recipes-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipesListComponent {
 
